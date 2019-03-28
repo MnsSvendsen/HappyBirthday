e8c1a453b23f9d5b006da30f5f421c59463375e2
Mathias <mathias.svendsen@proventus.no>
2019-03-28 08:03:52 +0100

    added names

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!', 'Patrick' ],
+			strings: [ 'HAPPY', 'BIRTHDAY!', 'Patrick & Mathias' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
