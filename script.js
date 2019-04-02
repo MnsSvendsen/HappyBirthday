f9d84a8fa77893ff6f416afa1bbd152a7629aeea
Mathias <mathias.svendsen@proventus.no>
2019-04-02 09:09:39 +0200

    added names

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!', 'https://www.rb.no/vis/personalia/greetings/y1Pdmwf4lPmtSnPazdssul' ],
+			strings: [ 'HAPPY', 'BIRTHDAY!', 'Zulfiqar Ali Haidari' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
