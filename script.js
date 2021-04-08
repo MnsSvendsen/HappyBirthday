968612971acd76b1b46b5ca4f7e65b23d2f82543
Mathias Svendsen <36676756+MnsSvendsen@users.noreply.github.com>
2021-04-08 07:00:51 +0200

    Update script.js
    New name

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 	hh = h / 2,
 
 	opts = {
-		strings: ['GRATULERRER', 'MED DAGEN!', 'Roger'],
+		strings: ['GRATULERRER', 'MED DAGEN!', 'Kathinka'],
 		charSize: 30,
 		charSpacing: 35,
 		lineHeight: 40,
