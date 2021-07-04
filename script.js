124ea197514683f7667b7c3a53e8e89362ebaa4e
Mathias Svendsen <36676756+MnsSvendsen@users.noreply.github.com>
2021-07-04 10:25:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 	hh = h / 2,
 
 	opts = {
-		strings: ['GRATULERRER', 'MED DAGEN!', 'Kathinka'],
+		strings: ['GRATULERRER', 'MED DAGEN!', 'Frk. Heimholt'],
 		charSize: 30,
 		charSpacing: 35,
 		lineHeight: 40,
